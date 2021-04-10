066cbecbdaf82412f688cac46d60b66305ebbae4
dayadam <dayadamuga@gmail.com>
2021-04-10 15:53:56 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace just_for_fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("New");
+
+            //if (args[0] == "Triangles")
+            // {
+            int[] var = { 2, 2, 2 };
+            Program Program = new Program();
+            Console.WriteLine(Program.Triangles(2, 2, 2));
+            int[] nums1 = { 1, 2, 3, 4, 5, 1 };
+            LinkedList<int> list1 = new LinkedList<int>(nums1);
+            Console.WriteLine(Program.Get5thFromLastLinkedList
+                (list1.First).Value);
+            // }
+        }
+
+        //private object Get5thFromLastLinkedList(LinkedListNode<int> first)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public string Triangles(int side1,
+            int side2, int side3)
+
+        {
+            //int firstItem;
+            //foreach (int item in lengthOfSides)
+            //{
+            //    firstItem = item;
+            //    Console.WriteLine(item);
+            //}
+
+            if (side1 == side2 && side1 == side3)
+            {
+                return "Equilateral";
+            }
+
+            if (side1 == side2 || side1 == side3
+                || side2 == side3)
+            {
+                return "Isosceles";
+            }
+
+            return "Scalene";
+        }
+        //LinkedList<int> linkedList
+        public LinkedListNode<int> Get5thFromLastLinkedList(
+            LinkedListNode<int> head)
+        {
+            if (head == null)
+                return null;
+
+            ArrayList nodeList = new ArrayList();
+            int length = 0;
+
+            var currentNode = head;
+            while (currentNode != null)
+            {
+                nodeList.Add(currentNode);
+                length++;
+                currentNode = currentNode.Next;
+            }
+
+            int count = nodeList.Count;
+            return (LinkedListNode<int>)nodeList[count - 5 - 1]; 
+
+            //LinkedListNode<int> prev = null;
+            //LinkedListNode<int> next = head.Next;
+            //while (next != null)
+            //{
+            //    head.Next = prev;
+            //    prev = head;
+            //    head = next;
+            //    next = head.Next;
+            //}
+
+            //int counter = 0;
+            //prev = null;
+            //next = head.next;
+            //while (counter < 5)
+            //{
+            //    head.next = prev;
+            //    prev = head;
+            //    head = next;
+            //    next = head.next;
+            //    counter++;
+            //}
+            //return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace just_for_fun
+{
+    public class ProgramTests
+    {
+        private readonly just_for_fun.Program _sut;
+
+        public ProgramTests()
+        {
+            _sut = new just_for_fun.Program();
+        }
+
+        [Theory]
+        [InlineData("Equilateral",  2, 2, 2 )]
+        [InlineData("Isosceles",  20, 2, 2 )]
+        [InlineData("Isosceles",  10, 3, 10 )]
+        [InlineData("Isosceles", 5, 5, 3 )]
+        [InlineData("Scalene",  4, 5, 3 )]
+        // [InlineData("Isosceles", new int[] { 5, 5, "yo" })]
+        public void SidesShouldEqualTriangle(
+            string expected,
+            int side1, int side2, int side3)
+        {
+            // int[] var = { 2, 2, 2 };
+            Assert.Equal(expected, _sut.Triangles(side1,
+                side2, side3));
+        }
+
+        [Theory]
+        // [InlineData(5, params )]
+        //[InlineData("Isosceles", 20, 2, 2)]
+        [MemberData(nameof(TestData))]
+
+        // [InlineData("Isosceles", new int[] { 5, 5, "yo" })]
+        public void FifthFromLastElInLinkedList(
+        LinkedListNode<int> expected, LinkedListNode<int> firstNode)
+        {
+            // int[] var = { 2, 2, 2 };
+            Assert.Equal(expected, _sut.
+                Get5thFromLastLinkedList(firstNode));
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            int[] nums1 = {1, 2, 3, 4, 5, 1, 1, 1, 1, 1};
+            LinkedList<int> list1 = new LinkedList<int>(nums1);
+            // int Node1 = List1.First;
+            yield return new object[] { list1.Find(5), list1.First };
+        }
+
+        //[Fact]
+        //public void ThreeEqualIntegersShouldReturnEquilateral()
+        //{
+        //    int[] var = { 2, 2, 2 };
+        //    Assert.Equal("Equilateral", _sut.Triangles(var));
+        //}
+    }
+}
